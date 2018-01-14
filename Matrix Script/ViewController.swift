6feb0d90ce77f8675614d98af4c4143279c10d02
Chun Wa Chow <tony@tonys-macbook-air.local>
2018-01-14 17:14:36 +0800

    Initial Commit

---
+++ Matrix Script/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Matrix Script
+//
+//  Created by Chun Wa Chow on 14/1/2018.
+//  Copyright © 2018 Tony Chow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
